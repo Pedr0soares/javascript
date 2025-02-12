67c03e93d592aed33d5a30feaf15571c0b613bfc
Pedr0soares <pedromlpd@gmail.com>
2025-02-12 08:55:54 -0300

    whle..

---
+++ Estrutura condicional/switch.js
@@ -0,0 +1,24 @@
+let opcao = 2
+switch (opcao){
+    case 1:
+        console.log("Você escolheu a opção 1")
+        break
+        case 2:
+            console.log("Você escolheu a opção 2")
+            break 
+            default:
+            console.log("Opçãao Invalida")
+}
+
+
+let letra = "A"
+switch(letra){
+    case "A":
+    console.log("Vogal")
+    break 
+    case "B":
+        console.log("Consoante")
+        break
+        case "C":
+            
+}
