fbf08e68c2959b1563edd55d0bb5c03712fd1348
Pedr0soares <pedromlpd@gmail.com>
2025-02-18 13:02:46 -0300

    array....

---
+++ .vscode/array/array.js
@@ -0,0 +1,137 @@
+let amigos = ["Carlos", "Joana", "Marcos", "Barbara", "Luana"]
+
+console.log(amigos[3])
+
+for(let i = 0; i < amigos.length; i++) {
+    console.log(amigos[i])
+}
+console.log("adicionando ao fim da lista")
+amigos.push("Fernanda")
+console.log(amigos)
+
+console.log("remove o primeiro da lista")
+amigos.shift("Carlos")
+console.log(amigos)
+
+console.log("remove o último da lista")
+amigos.pop("Launa")
+console.log(amigos)
+
+console.log("adiciona no inicio da lista")
+amigos.unshift("Raphael")
+console.log(amigos)
+
+
+
+
+
+
+//ex1
+let amigos1 = ["Carros", "Moana", "Batman"]
+
+console.log(amigos1[3])
+
+for(let i = 0; i < amigos1.length; i++) {
+    console.log(amigos[i])
+}
+console.log("remove o primeiro da lista")
+amigos1.shift("Carros")
+console.log(amigos1)
+
+//ex2
+
+
+
+let frutas = ["pera", "maça", "uva", "morango", "pitaia"]
+
+console.log(f[3])
+
+for(let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+}
+console.log("adicionando ao fim da lista")
+frutas.push("uva")
+console.log(frutas)
+
+
+//ex3
+
+let cores = ["azul", "vermelho", "cinza"]
+
+console.log(f[3])
+
+for(let i = 0; i < cores.length; i++) {
+    console.log(cores[i])
+}
+console.log("adicionando ao fim da lista")
+cores.push("laranja")
+console.log(cores)
+
+
+//ex4
+
+let num1 = ["1", "2", "3", "4"]
+
+console.log(f[3])
+
+for(let i = 0; i < num1.length; i++) {
+    console.log(num1[i])
+}
+console.log("adicionando ao fim da lista")
+num1.pop("4")
+console.log(num1)
+
+//ex5 
+let cidade = ["Suzano", "Mogi"]
+
+console.log(f[3])
+
+for(let i = 0; i < cidade.length; i++) {
+    console.log(cidade[i])
+}
+console.log("adiciona no inicio da lista")
+cidade.unshift("Poá")
+console.log(cidade)
+
+//ex6
+let animal = ["Tatu", "Leão","Cachorro"]
+
+console.log(f[3])
+
+for(let i = 0; i < animal.length; i++) {
+    console.log(animal[i])
+}
+console.log("remove o primeiro da lista")
+animal.shift("Tatu")
+console.log(animal)
+
+//ex7 
+let carros = ["Astra", "Palho","Uno","Ciena"]
+
+console.log(f[3])
+
+for(let i = 0; i < carros.length; i++) {
+    console.log(carros.length)
+}
+
+//ex8
+let vazio = ["3", "4", "5"]
+
+console.log(vazio[3])
+
+for(let i = 0; i < vazio.length; i++) {
+    console.log(vazio[i])
+}
+console.log("adiciona no inicio da lista")
+vazio.unshift("2")
+console.log(vazio)
+console.log("remove o último da lista")
+vazio.pop("5")
+console.log(amigos)
+console.log("remove o primeiro da lista")
+vazio.shift("2")
+console.log(vazio)
+for(let i = 0; i < vazio.length; i++) {
+    console.log(vazio[i])
+}
+console.log(vazio.length)
