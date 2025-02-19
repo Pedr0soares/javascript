7e9a04ba9155e2475eb1c192d7e9489f67214a72
Pedr0soares <pedromlpd@gmail.com>
2025-02-19 09:23:42 -0300

    222

--- .vscode/array/funções/declarativa.js
+++ .vscode/array/funções/declarativa.js
@@ -1,59 +1,64 @@
-// SEM PARAMERO
-function mostraMaensagem (){
-    console.log("Olá")
+// 
+function mostraMensagem() {
+    console.log("Olá");
 }
-fuction contarAteTres(){
-    for(let i = 1; i <= 3; i++){
-        console.log(i)
+
+// 
+function contarAteTres() {
+    for (let i = 1; i <= 3; i++) {
+        console.log(i);
     }
 }
-cortarAteTres()
+contarAteTres();
 
-function somar (a,b){
-    let resultado = a + b
-    return resultado
+// Função de soma 
+function somar(a, b) {
+    let resultado = a + b;
+    return resultado;
 }
 
-console.log(somar(3,7))
-console.log(somar (10,3))
-console.log(somar(8,125))
+console.log(somar(3, 7));
+console.log(somar(10, 3));
+console.log(somar(8, 125));
 
-function aoQuadrado(numero){
-    let quadrado = numero * numero
+//  
+function aoQuadrado(numero) {
+    let quadrado = numero * numero;
+    return quadrado;
 }
 
-console.log(aoQuadrado(5))
-console.log(aoQuadrado(8))
-console.log(aoQuadrado(251))
+console.log(aoQuadrado(5));
+console.log(aoQuadrado(8));
+console.log(aoQuadrado(251));
 
-
-//exc1
-function javaScript(){
-    console.log("Eu gosto de javaScript")
+// exc1
+function javaScript() {
+    console.log("Eu gosto de JavaScript");
 }
 
-//exc2
-function contarAteCinco(){
-    console.log("1")
-    console.log("2")
-    console.log("3")
-    console.log("4")
-    console.log("5")
+// exc2
+function contarAteCinco() {
+    console.log("1");
+    console.log("2");
+    console.log("3");
+    console.log("4");
+    console.log("5");
 }
 
-//exc3
-function saudacao(){
-    console.log("Olá, Pedro! Seja bem-vindo!")
+// exc3
+function saudacao() {
+    console.log("Olá, Pedro! Seja bem-vindo!");
 }
 
-//exc4
-function multiplicarDoisNumeros (a,b){
-    let resultado = a * b
-    return resultado
+// exc4
+function multiplicarDoisNumeros(a, b) {
+    let resultado = a * b;
+    return resultado;
 }
-console.log(multiplicarDoisNumeros(5,3))
 
-//exc5
+console.log(multiplicarDoisNumeros(5, 3));
+
+// exc5
 function verificarIdade(idade) {
     if (idade >= 18) {
         console.log("Maior de idade");
@@ -61,17 +66,18 @@ function verificarIdade(idade) {
         console.log("Menor de idade");
     }
 }
-//exc6
+
+// exc6
 function somar(num1, num2) {
     return num1 + num2;
 }
 
-//exc7
+// exc7
 function calcularAreaRetangulo(largura, altura) {
     return largura * altura;
 }
 
-//exc8
+// exc8
 function compararIdade(idades) {
     let maisVelho = idades[0];
 
